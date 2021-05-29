3344cc198e828d071241608a9c3ae74328e033e0
Eva <80411518+EVa-Belskaia@users.noreply.github.com>
2021-05-29 10:57:52 +0300

    Do'n fly two ball
    Второй шарик не летит...

--- CrazyBall.cpp
+++ CrazyBall.cpp
@@ -1,30 +1,77 @@
 #include "TXLib.h"
 
-void GoCraziBall( );
+void GoCraziBall        (int x, int y, int vx, int vy, int dt);
+void GoCraziBallControl (int x, int y, int vx, int vy, int dt);
+void Ball_Control       (int vx, int vy);
 
 int main()
     {
     txCreateWindow (1000, 700);
 
-    GoCraziBall( );
+    GoCraziBall        (800, 500, -5, -4, 1);
+    GoCraziBallControl (100, 100,  5,  3, 1);
 
     return 0;
     }
-
-void GoCraziBall( )
+//--------------------------------------------------------------------------------------------
+void GoCraziBallControl (int x, int y, int vx, int vy, int dt)
     {
-    int x  = 100, y = 100,
-        vx = 6,   vy = 4;
-
-        int dt = 1;
-
         txSetColor     (RGB (95, 200,   2), 2);
         txSetFillColor (RGB (95, 200, 250));
 
         while (!txGetAsyncKeyState (VK_ESCAPE))
         {
-        txSetColor     (RGB (2*x, 2*y, 128), 2);
-        txSetFillColor (RGB (  x,   y, 128));
+        txSetColor     (RGB (50, 100, 128), 2);
+        txSetFillColor (RGB ( 0,   128, 128));
+
+        txCircle (x, y, 20);
+
+        x += vx * dt;
+        y += vy * dt;
+
+        if (x > 1000)
+            {
+            vx = -vx;
+            x  = 1000;
+            }
+
+        if (y >  700)
+            {
+            vy = -vy;
+            y  = 700;
+            }
+
+        if (x < 0)
+            {
+            vx = -vx;
+            x  =   0;
+            }
+
+        if (y < 0)
+            {
+            vy = -vy;
+            y  =   0;
+            }
+        txSleep (10);
+        //Ball_Control (vx, vy);
+    if (txGetAsyncKeyState (VK_UP))    vy ++;
+    if (txGetAsyncKeyState (VK_DOWN))  vy --;
+    if (txGetAsyncKeyState (VK_LEFT))  vx --;
+    if (txGetAsyncKeyState (VK_RIGHT)) vx ++;
+    if (txGetAsyncKeyState (VK_SPACE)) vx = vy = 0;
+
+        }
+    }
+//--------------------------------------------------------------------------------------------
+void GoCraziBall (int x, int y, int vx, int vy, int dt)
+    {
+        txSetColor     (RGB (95, 200,   2), 2);
+        txSetFillColor (RGB (95, 200, 250));
+
+        while (!txGetAsyncKeyState (VK_ESCAPE))
+        {
+        txSetColor     (RGB (200, 50, 18), 2);
+        txSetFillColor (RGB (255, 128, 128));
 
         txCircle (x, y, 20);
 
@@ -56,10 +103,17 @@ void GoCraziBall( )
             }
         txSleep (10);
 
-        if (txGetAsyncKeyState (VK_UP))    vy ++;
-        if (txGetAsyncKeyState (VK_DOWN))  vy --;
-        if (txGetAsyncKeyState (VK_LEFT))  vx --;
-        if (txGetAsyncKeyState (VK_RIGHT)) vx ++;
-        if (txGetAsyncKeyState (VK_SPACE)) vx = vy = 0;
         }
     }
+//--------------------------------------------------------------------------------------------
+void Ball_Control (int vx, int vy)
+    {
+
+    if (txGetAsyncKeyState (VK_UP))    vy ++;
+    if (txGetAsyncKeyState (VK_DOWN))  vy --;
+    if (txGetAsyncKeyState (VK_LEFT))  vx --;
+    if (txGetAsyncKeyState (VK_RIGHT)) vx ++;
+    if (txGetAsyncKeyState (VK_SPACE)) vx = vy = 0;
+    }
+
+
